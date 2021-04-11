6d9cc41fe5cc6558571e5de82569b140cbf243b9
nat6 <nat6dv@gmail.com>
2021-04-11 23:10:07 +0300

    feat: add minesweeper function

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,51 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const result = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    const tempRes = [];
+
+    for (let k = 0; k < matrix[i].length; k++) {
+      let count = 0;
+
+      if (matrix[i - 1] !== undefined) {
+        if (matrix[i - 1][k - 1] === true) {
+          count++;
+        }
+        if (matrix[i - 1][k] === true) {
+          count++;
+        }
+        if (matrix[i - 1][k + 1] === true) {
+          count++;
+        }
+      }
+
+      if (matrix[i][k - 1] === true) {
+        count++;
+      }
+      if (matrix[i][k + 1] === true) {
+        count++;
+      }
+
+      if (matrix[i + 1] !== undefined) {
+        if (matrix[i + 1][k - 1] === true) {
+          count++;
+        }
+        if (matrix[i + 1][k] === true) {
+          count++;
+        }
+        if (matrix[i + 1][k + 1] === true) {
+          count++;
+        }
+      }
+
+      tempRes.push(count);
+    }
+    result.push(tempRes);
+  }
+  return result;
 }
 
 module.exports = minesweeper;
